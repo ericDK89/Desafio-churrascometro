22ca758b5ed4d857cd7531aef0cd798a2c322fb0
Eric Macedo <ericthr42@gmail.com>
2022-02-21 21:18:43 -0300

    window.onload function, calcular = function

--- script/script.js
+++ script/script.js
@@ -2,4 +2,28 @@
 //Cerveja - 1200 ml por pessoa + 6 horas - 2000 ml
 //Refrigerante/agua - 1000 ml por pessoa + 6 horas - 1500 ml
 
-//crianças valem por 0,5
+//Crianças valem por 0,5
+
+
+
+window.onload = function() {
+
+    let inputAdultos = document.getElementById("adultos")
+    let inputCriancas = document.getElementById("criancas")
+    let inputDuracao = document.getElementById("duracao")
+
+    let resultado = document.getElementById("resultado");
+
+
+    calcular = function() {
+        console.log("Calculando...")
+
+        let qtdAdultos = inputAdultos.value;
+        let qtdCriancas = inputCriancas.value;
+
+        let qtdTotalCarne = 400 * qtdAdultos + (400 / 2 * qtdCriancas);
+
+        console.log(qtdTotalCarne);
+    }
+
+}
